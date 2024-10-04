8b457e7f807079f3068841aefa5e69affd7ef275
Marcos <marcos.pauloferreirade.moraes@escola.pr.gov.br>
2024-10-04 09:55:23 -0300

    aula 2
    início aula 2

---
+++ Aula 2 condicional.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 6;
+let c = 7;
+
+let soma = a+b;
+
+if(soma > c){
+    console.log("O valor" + soma + "que o valor" + c);
+} else {
+    console.log("O valor" + soma + "é menor que o valor" + c)
+}
